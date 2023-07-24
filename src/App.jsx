8d574b1c543dfcad0aec005e28038b2173bc4824
ashish <invoxico.ashish@gmail.com>
2023-07-24 11:31:51 +0530

    Filter commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 import Searchresult from "./components/Searchresult";
+import Searchfun from "./components/Searchfunction/Searchfun";
 
 function App() {
   const [results, setResult] = useState([]);
 
   return (
-    <div className="App">
-      <div className="search-bar-container">
-        <SearchBar setResult={setResult} />
-        <Searchresult results={results} />
-      </div>
-    </div>
+    // <div className="App">
+    //   {/* <div className="search-bar-container">
+    //     <SearchBar setResult={setResult} />
+    //     <Searchresult results={results} />
+    //   </div> */}
+
+    // </div>
+    <Searchfun />
   );
 }
 
